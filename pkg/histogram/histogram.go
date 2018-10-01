69e8ca812d7d4dd65741c6cfabf4d28e6bbcd27e
Elliot Forbes <emforce@live.com>
2018-10-01 20:51:37 +0100

    Updates

--- pkg/histogram/histogram.go
+++ pkg/histogram/histogram.go
@@ -1,28 +1,40 @@
 package histogram
 
 type Histogram struct {
-	Buckets []Bucket
-}
-
-type Bucket struct {
-	Low    float64
-	High   float64
 	Values []float64
 }
 
-// GenerateHistogram generates a simple histogram in which
-// it buckets everything
+// GenerateHistogram generates a histogram based off the pixel 2D array passed into it
 func GenerateHistogram(pixels [][]float64) Histogram {
 	var histogram Histogram
+
+	// 1 generate grid
 
 	return histogram
 }
 
 // EuclidDistance returns the euclidean distance between two histograms
-// or an error
 func (h *Histogram) EuclidDistance(histogram Histogram) (float64, error) {
 	var distance float64
 	// distance = math.Sqrt(math.Pow((x.X-y.X), 2) + math.Pow((x.Y-y.Y), 2) + math.Pow((x.Z-y.Z), 2))
 	// return 0, distance
 	return distance, nil
+}
+
+// NormalizedEuclidDistance returns the normalized euclidean distance between two histograms
+func (h *Histogram) NormalizedEuclidDistance(histogram Histogram) (float64, error) {
+	var distance float64
+	return distance, nil
+}
+
+// AbsoluteValue returns the absolute value between two histograms
+func (h *Histogram) AbsoluteValue(histogram Histogram) (float64, error) {
+	var value float64
+	return value, nil
+}
+
+// ChiSquare returns the distance between two histograms calculated using the chi square statistic
+func (h *Histogram) ChiSquare(histogram Histogram) (float64, error) {
+	var sum float64
+	return sum, nil
 }
---
+++ pkg/video/video.go
@@ -0,0 +1,8 @@
+// Package video is a placeholder package for when
+// we start to look at realtime dynamic face recognition
+package video
+
+// Idea 1: Take in video stream, build histogram of faces within that video stream
+//         and give people the ability to label these faces dynamically.
+//         + Person 1 - circled in Red in the video is Elliot
+//         + Person 2 - circled in Green in the video is Sophie
